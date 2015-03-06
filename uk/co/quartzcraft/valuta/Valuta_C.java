b4b8498e556584e383f277a15f4a192dfdab353c
SRM <Jamie.valdez8@googlemail.com>
2015-03-06 17:30:34 +0000

    Update Valuta_C.java

--- uk/co/quartzcraft/valuta/Valuta_C.java
+++ uk/co/quartzcraft/valuta/Valuta_C.java
@@ -5,12 +5,12 @@
 #
 
 package uk.co.quartzcraft.valuta;
- 
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
- 
+
 public class Valuta_C implements CommandExecutor
 {
 
@@ -23,8 +23,65 @@ public class Valuta_C implements CommandExecutor
 			cs.sendMessage(ChatColor.GREEN+"The correct way: /valuta <add/remove/set> <player> <amount>");
 			return true;
 		}
-               
-		if (cs.hasPermission(valuta.use))
+		
+	if(args[0].equalsIgnoreCase("add"))
+	{
+		if(Valuta_M.hasAccount(args [1]));
+		{
+			cs.sendMessage(ChatColor.RED+"This player doesn't have an account!");
+			return true;
+		}
+		double amount = 0;
+		try
+		{
+			amount = double.parseDouble(args [2]);
+		}catch (Exception e)
+		{
+			cs.sendMessage(ChatColor.RED+"Please enter a correct amount.");
+			return true;
+		}
+		
+		Valuta_M.setBalance(args [1], Valuta_M.getBalance(args [1]) + amount);
+	}else if (args[0].equalsIgnoreCase("remove"))
+	{
+		if(Valuta_M.hasAccount(args [1]));
+		{
+			cs.sendMessage(ChatColor.RED+"This player doesn't have an account!");
+			return true;
+		}
+		double amount = 0;
+		try
+		{
+			amount = double.parseDouble(args [2]);
+		}catch (Exception e)
+		{
+			cs.sendMessage(ChatColor.RED+"Please enter a correct amount.");
+			return true;
+		}
+		
+		Valuta_M.setBalance(args [1], Valuta_M.getBalance(args [1]) - amount);
+	}else if (args[0].equalsIgnoreCase("set"))
+	{
+		if(Valuta_M.hasAccount(args [1]));
+		{
+			cs.sendMessage(ChatColor.RED+"This player doesn't have an account!");
+			return true;
+		}
+		double amount = 0;
+		try
+		{
+			amount = double.parseDouble(args [2]);
+		}catch (Exception e)
+		{
+			cs.sendMessage(ChatColor.RED+"Please enter a correct amount.");
+			return true;
+		}
+		
+		Valuta_M.setBalance(args [1], amount);
+	}
+		cs.sendMessage(ChatColor.RED+"That's an incorrect argument!");
 	}
 		return true;
 	}
+	
+	
