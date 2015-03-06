b9b4b55d6af39cad26b307929119a19da3c8b20a
SRM <Jamie.valdez8@googlemail.com>
2015-03-06 17:27:43 +0000

    Update Valuta.java

--- uk/co/quartzcraft/valuta/Valuta.java
+++ uk/co/quartzcraft/valuta/Valuta.java
@@ -3,3 +3,18 @@
 # Valuta.java
 # All Rights Reserved.
 #
+
+package uk.co.quartzcraft.valuta;
+
+public class Valuta
+	
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Valuta extends JavaPlugin
+{
+	public void onEnable()
+	{
+		getCommand("valuta").setExecutor(new ValutaCommand());
+	}
+
+}
