feac9e753a79534fa46427919cdc9de3753ce18a
SRM <Jamie.valdez8@googlemail.com>
2015-03-06 17:25:48 +0000

    Update Valuta_M.java

--- uk/co/quartzcraft/valuta/Valuta_M.java
+++ uk/co/quartzcraft/valuta/Valuta_M.java
@@ -5,20 +5,83 @@
 #
 
 package uk.co.quartzcraft.valuta;
- 
-import java.util.HashMap;
- 
-public class Valuta_M
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Valuta_C implements CommandExecutor
 {
-	public static HashMap<String, Double> balance = new HashMap<>();
 
-	public static void setBalance(String player, double amount)
+	@Override
+	public boolean onCommand(CommandSender cs, Command command, String S, String[] args)
 	{
-		balance.put(player, amount);
-	}
-
-	public static Double getBalance (String player)
+		if(args.length != 3)
+		{
+			cs.sendMessage(ChatColor.RED+"You can't do that.");
+			cs.sendMessage(ChatColor.GREEN+"The correct way: /valuta <add/remove/set> <player> <amount>");
+			return true;
+		}
+		
+	if(args[0].equalsIgnoreCase("add"))
 	{
-		return balance.get(player);
+		if(Valuta_M.hasAccount(args [1]));
+		{
+			cs.sendMessage(ChatColor.RED+"This player doesn't have an account!");
+			return true;
+		}
+		double amount = 0;
+		try
+		{
+			amount = double.parseDouble(args [2]);
+		}catch (Exception e)
+		{
+			cs.sendMessage(ChatColor.RED+"Please enter a correct amount.");
+			return true;
+		}
+		
+		Valuta_M.setBalance(args [1], Valuta_M.getBalance(args [1]) + amount);
+	}else if (args[0].equalsIgnoreCase("remove"))
+	{
+		if(Valuta_M.hasAccount(args [1]));
+		{
+			cs.sendMessage(ChatColor.RED+"This player doesn't have an account!");
+			return true;
+		}
+		double amount = 0;
+		try
+		{
+			amount = double.parseDouble(args [2]);
+		}catch (Exception e)
+		{
+			cs.sendMessage(ChatColor.RED+"Please enter a correct amount.");
+			return true;
+		}
+		
+		Valuta_M.setBalance(args [1], Valuta_M.getBalance(args [1]) - amount);
+	}else if (args[0].equalsIgnoreCase("set"))
+	{
+		if(Valuta_M.hasAccount(args [1]));
+		{
+			cs.sendMessage(ChatColor.RED+"This player doesn't have an account!");
+			return true;
+		}
+		double amount = 0;
+		try
+		{
+			amount = double.parseDouble(args [2]);
+		}catch (Exception e)
+		{
+			cs.sendMessage(ChatColor.RED+"Please enter a correct amount.");
+			return true;
+		}
+		
+		Valuta_M.setBalance(args [1], amount);
 	}
-}
+		cs.sendMessage(ChatColor.RED+"That's an incorrect argument!");
+	}
+		return true;
+	}
+	
+	
