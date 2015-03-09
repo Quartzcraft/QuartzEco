1e2888e6cce3e1d98b4d46c98cf7fc08b9c42222
SRM <Jamie.valdez8@googlemail.com>
2015-03-09 19:25:54 +0000

    Create api.java

---
+++ uk/co/quartzcraft/valuta/api.java
@@ -0,0 +1,23 @@
+package uk.co.quartzcraft.valuta;
+
+public class api
+{
+	private static Valuta plugin = Valuta_M.getPlugin();
+	
+	public static save saveBalances()
+	{
+		for(String p : Valuta_M.getBalanceMap().keySet())
+		{
+			plugin.getConfig().set("balance.+p, Valuta_M.getBalanceMap().get(p)");
+		}
+	}
+	
+	public static void loadBalances()
+	{
+		if(!plugin.getConfig().contains("balance")) return;
+		for(String S : plugin.getConfig().getConfigurationSection("balance".getKeys(false)))
+		{
+			Valuta_M.setBalance(s, plugin.getConfig().getDouble("balance."+s));
+		}
+	}
+}
